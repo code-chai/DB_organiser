dbdcec9a13cc38eaf294d35b5144d2edbcb01169
code-chai <jeremy.boyle1207@gmail.com>
2022-01-16 18:19:41 +0000

    Ommitted redundant code, wrapped in a function. Added README.md.

--- DB_organiser.py
+++ DB_organiser.py
@@ -2,56 +2,48 @@ import os
 import re
 
 # Place yourself in the directory with your Genbank (GCA) genome data folders.
-if os.path.exists("../GenomeDB") == False:
-    os.mkdir("../GenomeDB") 
-
-pattern = "(^GCA).*(fna$)"
+genfile_regex = "(^GCA).*(fna$)"
 woutboth = 0 # will count the no. folders without both files we need. 
-# denom can just be len(os.listdir())
 
-for item in os.listdir():
-    suffix = item.removeprefix("GCA_") 
-    print(suffix)
-    # .removeprefix() method is available in python 3.9+.
-    os.chdir(item)
-    print(f"Now in {os.getcwd()}. \n Files: {os.listdir()}")
-    the_whey = "./protein.faa"
-    destination = f"../../GenomeDB/{suffix}.faa"
+def DB_organiser(pattern):
+    global woutboth 
+    if os.path.exists("../GenomeDB") == False:
+        os.mkdir("../GenomeDB") 
+    if os.path.exists("../RASTthis") == False:
+        os.mkdir("../RASTthis") # the dir where we'll run MyRAST on. 
 
-    if os.path.exists(the_whey) and (re.search(pattern,file) for file in os.listdir()):
-        goodtogo = 1
-    else:
-        goodtogo = 0
-    
-    try:  
-        if goodtogo == 1:
-        # If the dataset includes both a protein.faa file and genome.fna file,
-            os.rename(the_whey, destination)
-            print(f"Moving {the_whey} to {destination}")
-        else:
-            woutboth += 1
-            # Why do some ncbi genome datasets come with a sequence report and not a sequence anyway?
-        os.chdir("..")
-    except PermissionError:
-        print("su-do you think you are? (Get permission)")
-    except OSError:
-        print("Not sure about that one, chief. (OSError)")
-
-print(f"GenomeDB: \n {os.listdir('../GenomeDB')}")
-
-if os.path.exists("../RASTthis") == False:
-    os.mkdir("../RASTthis") # the dir where we'll run MyRAST on. 
-
-if goodtogo == 1:
     for item in os.listdir():
-        suffix = item.removeprefix("GCA_")
-        os.chdir(item)
-        for file in os.listdir():
-            if re.search(pattern, file): 
-                os.rename(f"./{file}",f"../../RASTthis/{suffix}.fna")
-        os.chdir("..")
+        if item[0:3] == "GCA":
+            suffix = item.removeprefix("GCA_") 
+            # .removeprefix() method is available in python 3.9+.
+            os.chdir(item)
+            print(f"Now in {os.getcwd()}. \n Files: {os.listdir()}")
 
-print(f"RASTthis: \n {os.listdir('../RASTthis')}")
+            the_whey = "./protein.faa"
+            whey_dest = f"../../GenomeDB/{suffix}.faa"
+            faa_present = os.path.exists(the_whey)
+            for file in os.listdir():
+                if re.search(pattern, file):
+                    fna_present = True
+                    the_jean = f"./{file}"
+            jean_dest = f"../../RASTthis/{suffix}.fna"
 
-useful = woutboth/len(os.listdir())
-print("In this directory, %f datasets had both files we wanted.") %useful
+            if faa_present and fna_present:
+            # If the dataset includes both a protein.faa file and genome.fna file,
+                os.rename(the_whey, whey_dest)
+                print(f"Moving {the_whey} to {whey_dest}")
+                os.rename(the_jean, jean_dest)
+                print(f"Moving {the_jean} to {jean_dest}")
+            else:
+                woutboth += 1
+            os.chdir("..")
+
+    print(f"GenomeDB: \n {os.listdir('../GenomeDB')}") 
+    print(f"RASTthis: \n {os.listdir('../RASTthis')}")
+    
+
+DB_organiser(genfile_regex)
+
+useful = (round(1-(woutboth/len(os.listdir())),2))*100
+print(f"In this directory, {useful}% of the datasets had both files we wanted.")
+
---
+++ README.md
@@ -0,0 +1,8 @@
+# Hello! 
+`DB_organiser` is a script that helps with the database preparation stage of the EvoMining workflow. 
+It takes fna and faa files downloaded from NCBI Genbank accessions, standardises their naming and moves them about, the `.faa` files straight to the GenomeDB folder, and the `.fna` files to an intermediate folder that MyRAST can be run on to provide standard annotations.
+
+NB: As I am currently learning how to use EvoMining, I may have certain misconceptions about the input for the GenomeDB: using genome accessions instead of contigs/groups of contigs specified in the paper. If whole genome accessions cannot be used,I will fork and rework `DB_organiser`. 
+
+> Evomining: https://github.com/nselem/evomining
+> Original Paper: Cruz-Morales, P., Kopp, J.F., Martínez-Guerrero, C., Yáñez-Guerra, L.A., Selem-Mojica, N., Ramos-Aboites, H., Feldmann, J. and Barona-Gómez, F., 2016. Phylogenomic Analysis of Natural Products Biosynthetic Gene Clusters Allows Discovery of Arseno-Organic Metabolites in Model Streptomycetes. *Genome Biology and Evolution* [Online], 8(6), pp.1906–1916. Available from: https://doi.org/10.1093/gbe/evw125
